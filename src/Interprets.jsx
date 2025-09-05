6616404758ea6a431ecc132f523641f37858dc80
Matúš Pytel <110773242+Mathias5467@users.noreply.github.com>
2025-09-05 22:26:35 +0200

    loading composers

--- src/Interprets.jsx
+++ src/Interprets.jsx
@@ -1,26 +1,41 @@
 import data from './data/composers.json';
 import './Interprets.css';
 import play from '/assets/playCard.png';
+import { useState } from 'react';
 
 function Interprets() {
   const dataArray = data;
   const pathToImage = "https://mathias5467.github.io/listenio/assets/interprets/";
+  const [numberOfLoaded, setNumberLoaded] = useState(10);
 
+
+  const nacitajDalsie = () => {
+    setNumberLoaded((prev) => prev + 10);
+  }
 
   return (
-    <div className="content">
-      {dataArray.map((interpret, index) => {
-        const fileName = interpret.name.split(" ").join("_").toLowerCase(); // adjust extension
+    <div className="interprets">
+      <div className="content">
+      {dataArray.slice(0, numberOfLoaded).map((interpret, index) => {
+        const fileName = interpret.name.split(" ").join("_").toLowerCase();
         return (
           <div id={fileName} className="card" key={`${interpret.name}-${index}`}>
-            <img className="card-image" src={pathToImage + fileName + ".jfif"} alt={interpret.name} />
+            <img
+              className="card-image"
+              src={pathToImage + fileName + ".jfif"}
+              alt={interpret.name}
+            />
             <h2 className="card-title">{interpret.name}</h2>
             <div className="card-button">
-                <img className="card-button-img" src={play}></img>
+              <img className="card-button-img" src={play} alt="play button" />
             </div>
           </div>
         );
       })}
+      </div>
+      <div onClick={nacitajDalsie} className="load-button">
+        Načítať ďalšie
+      </div>
     </div>
   );
 }
